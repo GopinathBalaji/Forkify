c3809c6ab4f0fff4c25821ef8665313c8e5bf57d
GopinathBalaji <gopibalaji13@gmail.com>
2021-06-06 17:20:58 +0530

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,9 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+
+export const TIMEOUT_SEC = 10;
+
+export const RES_PER_PAGE = 10;
+
+export const KEY = 'dcc1061e-5d4b-4e0e-8ec8-57e325129bf8';
+
+export const MODAL_CLOSE_SEC = 2.5;
